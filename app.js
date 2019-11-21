5b065c9cc2e9ae1665128a0335a63f99e7665951
Paulo Dallastra <paulodallastra@Paulos-MacBook-Pro.local>
2019-11-20 19:27:49 -0700

    Adding Project to Heroku

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect('mongodb://localhost:27017/journalDB', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://admin-dallastra:@Tp88125707@cluster0-tk19t.mongodb.net/journalDB', {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema ({
     name: String,
@@ -146,6 +146,12 @@ app.post('/submit', function(req, res) {
         }
     })
 });
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, function () {
+    console.log("Server started Successfully");
 });
