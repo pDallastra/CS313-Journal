b7ddc9e15a9eb3c4ed8b8751dc08aafb3b308657
Paulo Dallastra <paulodallastra@Paulos-MacBook-Pro.local>
2019-11-22 00:24:03 -0700

    Almost There

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 // const _ = require('lodash');
 app.use(session({
@@ -26,9 +28,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect('mongodb+srv://admin-dallastra:@Tp88125707@cluster0-tk19t.mongodb.net/journalDB', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://admin-dallastra:@Tp88125707@cluster0-tk19t.mongodb.net/journalDB', {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true});
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
@@ -53,7 +55,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function (req, res) {
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         res.render('home');
     } else {
         res.render('checkUser');
@@ -61,48 +63,51 @@ app.get('/', function (req, res) {
 });
 
 app.get('/register', function (req, res) {
-    res.render('register', {
-    })
+    res.render('register', {})
 });
 
-app.get('/home', function(req, res){
-    const checkId = req.user.id;
-        if (req.isAuthenticated()){
-            Post.find({user_id: checkId}, function(err, posts){
-                res.render('home',{
-                    posts: posts
-                });
-            }) 
-                
-        } else {
-            res.redirect('login');
-        }
+app.get('/home', function (req, res) {
+    if (req.isAuthenticated()) {
+        const checkId = req.user.id;
+        Post.find({
+            user_id: checkId
+        }, function (err, posts) {
+            res.render('home', {
+                posts: posts
+            });
+        })
+
+    } else {
+        res.redirect('/');
+    }
 });
 
-app.post('/register', function(req, res){
+app.post('/register', function (req, res) {
 
-    User.register({username: req.body.username}, req.body.password, function(err, user){
-        if(err) {
+    User.register({
+        username: req.body.username
+    }, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
             res.redirect('/register');
-        }else {
-            passport.authenticate('local')(req, res, function(){
+        } else {
+            passport.authenticate('local')(req, res, function () {
                 res.redirect('home');
             })
         }
     });
 });
 
-app.post('/login', function(req, res){
+app.post('/login', function (req, res) {
     const user = new User({
         username: req.body.username,
         password: req.body.password
     });
-    req.login(user, function(err){
+    req.login(user, function (err) {
         if (err) {
             console.log(err);
         } else {
-            passport.authenticate('local')(req, res, function(){
+            passport.authenticate('local')(req, res, function () {
                 res.redirect('home');
             });
         }
@@ -110,7 +115,18 @@ app.post('/login', function(req, res){
 });
 
 app.get('/login', function (req, res) {
-    res.render('login')
+    res.render('/')
+});
+
+app.post('/delete', function (req, res) {
+    const id = req.body.deleteItem;
+    Post.findByIdAndRemove(id, function(err){
+        const checkId = req.user.id;
+        Post.find({user_id: checkId}, function(err, posts){
+            res.render('home',{
+                posts: posts
+            });
+    })})
 });
 
 app.get('/logout', function (req, res) {
@@ -119,27 +135,27 @@ app.get('/logout', function (req, res) {
 });
 
 app.get('/submit', function (req, res) {
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         res.render('home');
     } else {
-        res.render('checkUser');
+        res.render('/');
     }
-    
+
 });
 
-app.post('/submit', function(req, res) {
-    User.findById(req.user.id, function(err, foundUser){
-        if (err){
+app.post('/submit', function (req, res) {
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
             console.log(err);
             res.redirect('/');
         } else {
-            if(foundUser) {
-                const newPost = new Post ({
+            if (foundUser) {
+                const newPost = new Post({
                     title: req.body.title,
                     content: req.body.content,
                     user_id: req.user.id
                 });
-                newPost.save(function(){
+                newPost.save(function () {
                     res.redirect('home');
                 })
             }
@@ -148,7 +164,7 @@ app.post('/submit', function(req, res) {
 });
 
 let port = process.env.PORT;
-if(port == null || port == ""){
+if (port == null || port == "") {
     port = 3000;
 }
 
